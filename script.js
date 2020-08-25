8abf441de5bf088f798ecdca8e19ba549bd86fcc
Angel <angelarciniaga@gmail.com>
2020-08-25 00:09:40 -0400

    Desafio terminado

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
+
 let persona = prompt("Por vavor ingrese un numero del 1 al 6", "1");
 
-if (persona == 1){
-    let opcion = prompt("Elije un número del 1 al 100");
-    if (opcion <= 100){
-        for (let i = 0; i<= opcion; i++){
-            console.log("Número "+ i);
+switch (persona){
+    case '1':
+        let opcion = prompt("Elije un número del 1 al 100");
+        if (opcion <= 100){
+            for (let i = 0; i<= opcion; i++){
+                console.log("Número "+ i);
+            }
+        }else {
+            alert("No es un numero valido");
         }
-    }else {
-        alert("No es un numero valido");
-    }
-} else if(persona == 2){
-    let opcion = prompt("¿De qué color es el cabalo blanco de Napoleón?");
-    while (opcion != "blanco")
-        opcion = prompt("¿De qué color es el cabalo blanco de Napoleón?");
-}else if (persona == 3){
-    let mate = prompt("Nota de matematica");
-    let fisica = prompt("Nota de Física");
-    let ciencia = prompt("Nota de Ciencias");
-    let promedio = (parseFloat(mate)+parseFloat(fisica)+parseFloat(ciencia))/3;
-    alert("Su promedio es: " + parseInt(promedio));
-}else if (persona == 4){
+        break;
+
+    case '2':
+        let caballo = prompt("¿De qué color es el cabalo blanco de Napoleón?");
+        while (caballo != "blanco")
+            caballo = prompt("¿De qué color es el cabalo blanco de Napoleón?");
+            console.log(caballo);
+        break;
+
+    case '3':
+        let mate = prompt("Nota de matematica");
+        let fisica = prompt("Nota de Física");
+        let ciencia = prompt("Nota de Ciencias");
+        let promedio = (parseFloat(mate)+parseFloat(fisica)+parseFloat(ciencia))/3;
+        alert("Su promedio es: " + Math.round(promedio));
+        break;
+
+    case '4':
+        let frutas = prompt('Escribe el nombre de 3 Frutas', "pera, manzana, banana");
+        let arrayFrutas = frutas.split(', ');
+        /* console.log(arrayFrutas); */
+        for (const interator of arrayFrutas) {
+            if (interator == "manzana") {
+                continue
+            }
+            console.log(interator);
+        }
+        break;
+
+    case '5':
+        let nombre = prompt('Escribe tu nomnre', 'Angel');
+        let vocales = nombre.match(/[aeiou]/gi).length;
+        let consonantes = nombre.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
+        console.log(nombre);
+        console.log(vocales);
+        console.log(consonantes);
+        alert(`La cantida de vocales son: ${vocales} La cantidad de consonantes son: ${consonantes}`);
+        break;
+
+    case '6':
+        alert('Gracias. Chao!!');
+        break;
+}
+
+
+//Opción #4
+/* else if (persona == 4){
     let frutas = prompt("Escribe al menos 3 Frutas")
-        if (frutas == manzana) {
-            break;
-        }
-        console.log(frutas);
-    }
+    for (frutas)
+      
+    } */
